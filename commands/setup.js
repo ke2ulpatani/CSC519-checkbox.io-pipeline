a37f9c9d398a915d6df9d02262f0d5e796f44eff
Anshul Jethvani <ajethva@ncsu.edu>
2020-02-19 20:13:13 -0500

    change ip address to variable

--- commands/setup.js
+++ commands/setup.js
@@ -5,6 +5,7 @@ const os = require('os');
 
 const scpSync = require('../lib/scp');
 const sshSync = require('../lib/ssh');
+const JENKINS_IP = "192.168.33.20";
 
 exports.command = 'setup';
 exports.desc = 'Provision and configure the configuration server';
@@ -34,23 +35,23 @@ async function run(privateKey) {
     console.log(chalk.greenBright('Installing jenkins server!'));
 
     console.log(chalk.blueBright('Provisioning configuration server...'));
-    let result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    let result = child.spawnSync(`bakerx`, `run jenkins-srv bionic --ip ${JENKINS_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Installing privateKey on jenkins server'));
     let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
-    result = scpSync (identifyFile, 'vagrant@192.168.33.20:/home/vagrant/.ssh/mm_rsa');
+    result = scpSync (identifyFile, `vagrant@${JENKINS_IP}:/home/vagrant/.ssh/mm_rsa`);
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Running init script...'));
-    result = sshSync('/bakerx/pipeline/server-init.sh', 'vagrant@192.168.33.20');
+    result = sshSync('/bakerx/pipeline/server-init.sh', `vagrant@${JENKINS_IP}`);
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     let filePath = '/bakerx/pipeline/playbook.yml';
     let inventoryPath = '/bakerx/pipeline/inventory.ini';
 
     console.log(chalk.blueBright('Running ansible script...'));
-    result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath}`, 'vagrant@192.168.33.20');
+    result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath}`, `vagrant@${JENKINS_IP}`);
     if( result.error ) { process.exit( result.status ); }
 
 }
