a6564d6569942773530e51c2a0ada80d2de609d5
Maharsh Patel <mkpatel5@ncsu.edu>
2020-04-20 22:25:36 -0400

    Skeleton for prod up command

---
+++ commands/produp.js
@@ -0,0 +1,38 @@
+const child = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+const os = require('os');
+
+const scpSync = require('../lib/scp');
+const sshSync = require('../lib/ssh');
+const JENKINS_IP = "192.168.33.20";
+
+exports.command = 'prod up';
+exports.desc = 'Provision cloud instances and control plane';
+exports.builder = yargs => {
+    yargs.options({
+    });
+};
+
+
+exports.handler = async argv => {
+    const {} = argv;
+
+    (async () => {
+
+        await run();
+
+    })();
+
+};
+
+async function run() {
+
+    let filePath = '/bakerx/production/playbook.yml';
+    let inventoryPath = '/bakerx/pipeline/inventory.ini';
+
+    console.log(chalk.blueBright('Running ansible script...'));
+    result = sshSync(`/bakerx/pipeline/run-ansible.sh ${filePath} ${inventoryPath}`, `vagrant@${JENKINS_IP}`);
+    if( result.error ) { process.exit( result.status ); }
+
+}
